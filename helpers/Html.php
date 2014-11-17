f0b5bee72e86853c72ed8a2d23ed148e3300d67f
Nicola Tomassoni <nicola@digisin.it>
2014-11-17 12:54:34 +0100

    added fLabel() Helper

--- helpers/Html.php
+++ helpers/Html.php
@@ -15,6 +15,17 @@ namespace nonzod\foundation\helpers;
  */
 class Html extends \yii\helpers\Html {
 
+  const FLOAT_LEFT = 'left';
+  const FLOAT_RIGHT = 'right';
+  const CLEAR_FLOAT = 'clearfix';
+  const HIDDEN = 'hide';
+  const BORDER_RADIUS = 'radius';
+  const BORDER_ROUND = 'round';
+  const TYPE_SECONDARY = 'secondary';
+  const TYPE_SUCCESS = 'success';
+  const TYPE_WARNING = 'warning';
+  const TYPE_ALERT = 'alert';
+
   /**
    * Generates an icon.
    *
@@ -29,6 +40,34 @@ class Html extends \yii\helpers\Html {
     $class = isset($options['class']) ? ' ' . $options['class'] : '';
     $options['class'] = $prefix . $icon . $class;
     return static::tag($tag, '', $options);
+  }
+
+  /**
+   * Generates a label.
+   *
+   * @param string $content the label content
+   * @param string $type the label type - default is empty
+   *  - is one of [[self::TYPE_SECONDARY]], [[self::TYPE_SUCCESS]], [[self::TYPE_WARNING]], [[self::TYPE_ALERT]]
+   * @param string $border the border type - defaults is empty
+   *  - is one of [[self::BORDER_RADIUS]], [[self::BORDER_ROUND]]
+   * @param array $options html options for the label container
+   * @param string $tag the label container tag - defaults to 'span'
+   *
+   * Example(s):
+   * ~~~
+   * echo Html::fLabel('Regular');
+   * echo Html::fLabel('Primary', Html::TYPE_PRIMARY);
+   * echo Html::fLabel('Success round', Html::TYPE_SUCCESS, Html::BORDER_ROUND);
+   * ~~~
+   *
+   * @see http://getbootstrap.com/components/#labels
+   */
+  public static function fLabel($content, $type = '', $border = '', $options = [], $tag = 'span') {
+    Html::addCssClass($options, 'label');
+    Html::addCssClass($options, $type);
+    Html::addCssClass($options, $border);
+
+    return static::tag($tag, $content, $options);
   }
 
 }
